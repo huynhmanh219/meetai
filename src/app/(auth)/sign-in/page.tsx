543d6d27541cdd5d7fe85b68a55b19ce23e914e4
huynhmanh219 <135082065+huynhmanh219@users.noreply.github.com>
2025-11-04 21:53:47 +0700

    authentication-ui

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+const Page = () => {
+    return (
+    <>
+      <SignInView></SignInView>
+    </>  );
+}
+ 
+export default Page;
+
+
+//http://localhost:3000/sign-in
