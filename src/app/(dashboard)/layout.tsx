41abea5f35c815c6f63872f98fabcce9f7125135
huynhmanh219 <135082065+huynhmanh219@users.noreply.github.com>
2025-11-06 14:41:24 +0700

    dashboard-sidebar

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
+
+interface props{
+    children:React.ReactNode
+}
+const Layout = ({children}:props) => {
+    return ( 
+        <SidebarProvider>
+            <DashboardSidebar></DashboardSidebar>
+            <main className="flex flex-col h-screen w-screen bg-muted">
+             {children}
+            </main>
+        </SidebarProvider>
+     );
+}
+ 
+export default Layout;
